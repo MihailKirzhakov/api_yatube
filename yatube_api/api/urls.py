9d0f4c892ba45c05fc0ec5bb1bb52d2d7da8501f
Михаил Киржаков <kirzhakovma@mail.ru>
2024-06-19 22:23:25 +0300

    "Moved API_VERSION from settings.py to urls.py and updated URLs to use the new variable"

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,22 +1,25 @@
-from django.conf import settings
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views
 
 from .views import GroupViewSet, PostViewSet, CommentViewSet
 
+
+API_VERSION = 'v1'
+
+
 router = routers.DefaultRouter()
 
 router.register(
-    f'{settings.API_VERSION}/groups', GroupViewSet, basename='groups'
+    f'{API_VERSION}/groups', GroupViewSet, basename='groups'
 )
-router.register(f'{settings.API_VERSION}/posts', PostViewSet, basename='posts')
+router.register(f'{API_VERSION}/posts', PostViewSet, basename='posts')
 router.register(
-    rf'{settings.API_VERSION}/posts/(?P<post_id>\d+)/comments',
+    rf'{API_VERSION}/posts/(?P<post_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
 
 urlpatterns = [
-    path(f'{settings.API_VERSION}/api-token-auth/', views.obtain_auth_token),
+    path(f'{API_VERSION}/api-token-auth/', views.obtain_auth_token),
     path('', include(router.urls)),
 ]
